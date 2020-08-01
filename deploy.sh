6feabdf75cab074e7010483e94a49e0dabe823c8
hefeng6500 <2443992009@qq.com>
2020-08-01 21:51:11 +0800

    修改部署配置

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hefeng6500/blog.git master:gh-pages
+git push -f git@github.com:hefeng6500/advanced-front-end.git master
 
 cd -
